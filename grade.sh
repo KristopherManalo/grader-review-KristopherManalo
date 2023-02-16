65a5cec6db61a5a770989db0d0a5379fa32d8983
KristopherManalo <kristophermmanalo@gmail.com>
2023-02-16 14:35:23 -0800

    updated grade.sh

--- grade.sh
+++ grade.sh
@@ -13,4 +13,4 @@ fi
 echo 'Finished cloning'
 
 javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar student-submission/*.java
-java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar  org.junit.runner.JUnitCore TestListExamples
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar  org.junit.runner.JUnitCore student-submissionTestListExamples
