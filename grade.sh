0d654ba47f4b3be7d0f6011029c8a57c869e3643
KristopherManalo <kristophermmanalo@gmail.com>
2023-02-16 15:11:47 -0800

    updated grade.sh

--- grade.sh
+++ grade.sh
@@ -1,16 +1,19 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+WINCPATH='".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"'
 
-rm -rf student-submission
-git clone $1 student-submission
+# set -e
 
+rm -rf student-submission/
+git clone $1 student-submission/
+echo 'Finished cloning'
 if [[ -f student-submission/ListExamples.java ]]
 then
     cp ./TestListExamples.java student-submission/
+    cp -r lib student-submission/
 else
     exit
 fi
 
-echo 'Finished cloning'
-
-javac -cp $CPATH student-submission/*.java
-java -cp $CPATH org.junit.runner.JUnitCore student-submission/TestListExamples
+cd student-submission
+javac -cp ";lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
+java -cp ";lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" org.junit.runner.JUnitCore TestListExamples
