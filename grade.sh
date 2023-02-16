4f2e0edae3efeff57f26f0f4bf33d32379daabf6
KristopherManalo <kristophermmanalo@gmail.com>
2023-02-16 14:20:20 -0800

    updated grade.sh

--- grade.sh
+++ grade.sh
@@ -2,4 +2,10 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 git clone $1 student-submission
+
+if [[-f student-submission/ListExamples.java]]
+then
+else
+fi
+
 echo 'Finished cloning'
